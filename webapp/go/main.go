fe5dbc1f10db557c14aabcf3d01187e19e76c9ab
pikachu0310 <pikachu13711@gmail.com>
2024-11-17 01:17:16 +0900

    ✨ dropProbabilityを0に変更

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1090,7 +1090,7 @@ func getTrend(c echo.Context) error {
 // ISUからのコンディションを受け取る
 func postIsuCondition(c echo.Context) error {
 	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
-	dropProbability := 0.9
+	dropProbability := 0.0
 	if rand.Float64() <= dropProbability {
 		return c.NoContent(http.StatusAccepted)
 	}
